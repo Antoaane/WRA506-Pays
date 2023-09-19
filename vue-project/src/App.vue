e1fcd2ce94de19664e2bb2aa4a95439f860749ba
Antoaane <antoine.lauzis@etudiant.univ-reims.fr>
2023-09-19 11:02:30 +0200

    feat: new compoenent & average

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,14 +1,26 @@
 <script setup>
-  const symfony = 18
-  const integration = 16
-  const reseau = 13
+  import TitleText from './components/TitleText.vue'
+
+  const symfony = 4
+  const integration = 7
+  const reseau = 9
+  const cultureArtistique = 8
+  const communication = 6
+
+  const average = (symfony + integration + reseau + cultureArtistique + communication) / 5
 </script>
 
 <template>
+  <TitleText />
+
   <ul>
     <li>Note sur Symfony : {{ symfony }}</li>
     <li>Note en intégration : {{ integration }}</li>
     <li>Note en réseau : {{ reseau }}</li>
+    <li>Note en culture artistique : {{ cultureArtistique }}</li>
+    <li>Note en communication : {{ communication }}</li>
+    <br>
+    <li>Moyenne générale : {{ average }}</li>
   </ul>
 </template>
 
