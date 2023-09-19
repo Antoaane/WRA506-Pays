aa4941cc179dcfebf68ce5e35ccc9e1680c83d37
Antoaane <antoine.lauzis@etudiant.univ-reims.fr>
2023-09-19 11:27:20 +0200

    fix: column

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -23,30 +23,45 @@
 <template>
   <TitleText />
 
-  <ul>
-    <li>Note sur Symfony : {{ symfony }}</li>
-    <li>Note en intégration : {{ integration }}</li>
-    <li>Note en réseau : {{ reseau }}</li>
-    <li>Note en culture artistique : {{ cultureArtistique }}</li>
-    <li>Note en communication : {{ communication }}</li>
-    <br>
-    <li>Moyenne générale : {{ average }}</li>
-  </ul>
+  <div class="container">
+    <ul>
+      <li>Note sur Symfony : {{ symfony }}</li>
+      <li>Note en intégration : {{ integration }}</li>
+      <li>Note en réseau : {{ reseau }}</li>
+      <li>Note en culture artistique : {{ cultureArtistique }}</li>
+      <li>Note en communication : {{ communication }}</li>
+      <br>
+      <li>Moyenne générale : {{ average }}</li>
+    </ul>
 
-  <table>
-    <tr>
-      <th>Matière</th>
-      <th>Note</th>
-    </tr>
-    <tr v-for="item in table">
-      <td>{{ item.name }}</td>
-      <td>{{ item.note }}</td>
-    </tr>
-  </table>
+    <table>
+      <tr>
+        <th>Matière</th>
+        <th>Note</th>
+      </tr>
+      <tr v-for="item in table">
+        <td>{{ item.name }}</td>
+        <td>{{ item.note }}</td>
+      </tr>
+    </table>
+  </div>
+  
 </template>
 
 <style scoped>
 li {
   color: red;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+table {
+  margin-top: 2rem;
+  border-collapse: collapse;
+  border: 1px solid black;
 }
 </style>
