80ee638e3b2eb95a270919aa39ee32cc17479e47
Antoaane <antoine.lauzis@etudiant.univ-reims.fr>
2023-09-19 16:00:40 +0200

    feat: listing pays api

--- vue-project/src/views/Pays.vue
+++ vue-project/src/views/Pays.vue
@@ -1,58 +1,21 @@
 <script setup>
-    const pays = [
-        {
-            name: 'France',
-            id: 1
-        },
-        {
-            name: 'Espagne',
-            id: 2
-        },
-        {
-            name: 'Italie',
-            id: 3
-        },
-        {
-            name: 'Allemagne',
-            id: 4
-        },
-        {
-            name: 'Angleterre',
-            id: 5
-        },
-        {
-            name: 'Portugal',
-            id: 6
-        },
-        {
-            name: 'Belgique',
-            id: 7
-        },
-        {
-            name: 'Suisse',
-            id: 8
-        },
-        {
-            name: 'Autriche',
-            id: 9
-        },
-        {
-            name: 'Luxembourg',
-            id: 10
-        }
-    ]
+    import { onMounted, ref } from 'vue'
+    import axios from 'axios'
+
+    let data = ref('')
+
+    onMounted(async () => {
+    const response = await axios.get('https://restcountries.com/v3.1/all')
+    data.value = response.data
+    })
 </script>
 
 <template>
     <section>
         <h1>Pays</h1>
-        <ul>
-            <li v-for="pays in pays" :key="pays.id">
-                <router-link :to="`/pays/${pays.name}`">
-                    {{ pays.name }}
-                </router-link>
-            </li>
-        </ul>
+        <p v-for="pays in data" :key="pays.name.common">
+            {{ pays.name.common }}
+        </p>
     </section>
 </template>
 
